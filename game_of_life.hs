0c46c7a530716cb38da338810a34b86e2d34c4fd
gauravmi1 <gmi3024@gmail.com>
2020-11-21 22:24:22 +0530

    [Gaurav] rewrite gol

--- game_of_life.hs
+++ game_of_life.hs
@@ -1,56 +1,31 @@
-neighbours :: (Int, Int) -> [(Int, Int)]
-neighbours (x, y) = [(x+1, y), (x-1, y), (x, y+1), (x, y-1), (x+1, y+1), (x-1, y-1)]
+data Cell = DeadCell { x, y :: Int } | Cell { x, y :: Int } deriving (Show, Eq)
 
-intersect :: (Eq a) => [a] -> [a] -> [a]
-intersect [] _ = []
-intersect _ [] = []
-intersect (x:xs) ys
-  | x `elem` ys = x: intersect xs ys
-  | otherwise = intersect xs ys
+top, bottom, left, right :: Cell -> Cell
+top cell = Cell (x cell) (y cell + 1)
+bottom cell = Cell (x cell) (y cell - 1)
+left cell = Cell (x cell - 1) (y cell)
+right cell = Cell (x cell + 1) (y cell)
 
-lonelinessRule :: [(Int, Int)] -> (Int, Int) -> Bool
-lonelinessRule aliveCells cell
-  | aliveNeighbours  < 2 = False
-  | otherwise            = True
-  where aliveNeighbours =  length (intersect (neighbours cell) aliveCells)
+isAlive :: Cell -> Bool
+isAlive cell = cell `elem` aliveCells
 
-overcrowdingRule :: [(Int, Int)] -> (Int, Int) -> Bool
-overcrowdingRule aliveCells cell
-  | aliveNeighbours  > 3 = False
-  | otherwise            = True
-  where aliveNeighbours =  length $ intersect (neighbours cell) aliveCells
+neighbors :: Cell -> [Cell]
+neighbors cell =
+  [top cell, bottom cell, left cell, right cell, left $ top cell, top $ right cell, bottom $ left cell, bottom $ right cell]
 
-noChangeRule :: [(Int, Int)] -> (Int, Int) -> Bool
-noChangeRule aliveCells cell
-  | alive && optimumCrowd     = True
-  | not alive && optimumCrowd = False
-  | otherwise                 = False
-  where alive =  cell `elem` aliveCells
-        optimumCrowd = aliveNeighbours == 3 ||  aliveNeighbours == 2
-        aliveNeighbours =  length $ intersect (neighbours cell) aliveCells
+aliveNeighbors :: Cell -> Int
+aliveNeighbors cell = length $ filter isAlive (neighbors cell)
 
-deadToAliveRule :: [(Int, Int)] -> (Int, Int) -> Bool
-deadToAliveRule aliveCells cell
-  | isDead && aliveNeighbours == 3 = True
-  | otherwise                      = False
-  where isDead = not (cell `elem`\ aliveCells)
-        aliveNeighbours = length $ intersect (neighbours cell) aliveCells
+loneliness, overcrowding, unchanged :: Cell -> Bool
+loneliness cell   = aliveNeighbors cell < 2
+overcrowding cell = aliveNeighbors cell > 3
+unchanged cell    = let length = aliveNeighbors cell in length == 2 || length == 3
 
-deadToAlive :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
-deadToAlive _ [] = []
-deadToAlive aliveCells (x:xs) =
-  (deadToAlive aliveCells xs) ++ (filter (deadToAliveRule aliveCells) (neighbours x))
+aliveCells = [Cell 1 1]
 
-rules  = [noChangeRule, overcrowdingRule, lonelinessRule]
-
-blockPattern   :: [(Int, Int)]
-boatPattern    :: [(Int, Int)]
-blinkerPattern :: [(Int, Int)]
-toadPattern    :: [(Int, Int)]
-
-blockPattern   = [(1,1), (1,2), (2,1), (2,2)]
-boatPattern    = [(0, 1),(1, 0),(2, 1),(0, 2),(1, 2)]
-blinkerPattern = [(1, 1),(1, 0),(1, 2)]
-toadPattern    = [(1, 1),(1, 2),(1, 3),(2, 2),(2, 3),(2, 4)]
-
-runBlockPattern (x:xs) = any (==True) [r blockPattern x | r<- rules]
+evolve :: Cell -> Cell
+evolve cell
+  | loneliness cell   = DeadCell (x cell) (y cell)
+  | overcrowding cell = DeadCell (x cell) (y cell)
+  | unchanged cell    = cell
+  | otherwise         = cell
