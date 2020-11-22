acff3e7eb5d895c7aefbb6556f16a8c4cdd3f71b
gauravmi1 <gmi3024@gmail.com>
2020-11-22 13:25:45 +0530

    [Gaurav] refactor rover

--- rover.hs
+++ rover.hs
@@ -1,29 +1,25 @@
 data Direction = N | E | W | S deriving (Show, Eq, Read)
 
-leftD :: Direction -> Direction
-leftD d
-  | d == N = W
-  | d == W = S
-  | d == S = E
-  | d == E = N
+data State = State {
+  leftD:: Direction,
+  rightD:: Direction
+} deriving (Show, Read, Eq)
 
-rightD :: Direction -> Direction
-rightD d
-  | d == N = E
-  | d == E = S
-  | d == S = W
-  | d == W = N
+state N = State W E
+state W = State S N
+state S = State E W
+state E = State N S
 
 data Position = Position {x :: Int, y::Int} deriving (Show, Read)
 data Rover = Rover { position :: Position, direction :: Direction} deriving (Show)
 
 left :: Rover -> Rover
+left (Rover position direction) = Rover position (leftD $ state direction)
+
 right :: Rover -> Rover
+right (Rover position direction) = Rover position (rightD $ state direction)
+
 move :: Rover -> Rover
-
-left r = Rover (Position (x (position r)) (y (position r))) (leftD (direction r))
-right r = Rover (Position (x (position r)) (y (position r))) (rightD (direction r))
-
 move (Rover (Position x y) direction)
   | direction == N = Rover (Position x (y+1)) direction
   | direction == E = Rover (Position (x+1) y) direction
@@ -36,7 +32,6 @@ rover :: OriginX -> OriginY -> Direction -> Rover
 rover x y direction = Rover (Position x y) direction
 
 run :: Rover -> String -> Rover
-
 run r [] = r
 run rover (x:xs)
   | x == 'M' = run (move rover) xs
@@ -50,4 +45,3 @@ main = do
   let d = read inputD :: Direction
   let rover  = Rover (Position x y) d
   print $ run rover pattern
-
